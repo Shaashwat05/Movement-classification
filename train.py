055762b2ed8bc23251bbcf3a48247e2c4d8d45c1
Shaashwat <shaas2000@gmail.com>
2020-08-17 22:11:48 +0530

    model

--- train.py
+++ train.py
@@ -1,11 +1,17 @@
 import tensorflow as tf 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,MaxPooling1D,Softmax
+from tensorflow.keras.layers import Dense,MaxPooling1D,Softmax, LSTM, Dropout
+from tensorflow.keras import utils
 import numpy as np
 import pickle
 import glob
 import pandas as pd
 
+gpus = tf.config.experimental.get_visible_devices("GPU")
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu,True)
+    print("hi")
+    
 info = pd.read_excel('cmu-mocap-index-spreadsheet.xls')
 #info = info.iloc[1:]
 info = info.drop(['SUBJECT from CMU web database'], axis=1)
@@ -24,10 +30,10 @@ jump = jump['MOTION'].values.tolist()
 
 tot = walk + run + jump
 seq = []
-seq_len = 300
+seq_len = 150
 
 for ex in tot:
-    pickle_in = open('/home/shaashwatlobnikki/Desktop/pose_classification/pickle_data/'+ ex +'.pickle',"rb")
+    pickle_in = open('/home/shaashwatlobnikki/Desktop/movement_classification/pickle_data/'+ ex +'_worldpos.pickle',"rb")
     data = pickle.load(pickle_in)
 
     seq.append(data)
@@ -37,12 +43,33 @@ for fileno in range(len(seq)):
     #seq[fileno] = seq[fileno][:-(len(seq[fileno])%300)]
     for i in range(0,len(seq[fileno])-seq_len,1):
         x.append(seq[fileno][i:i+seq_len])
+       
         if(tot[fileno] in walk):
-            y.append('walk')
+            y.append(1)
         elif(tot[fileno] in run):
-            y.append('run')
+            y.append(2)
         else:
-            y.append('jump')
+            y.append(3)
+
+X= np.array(x)
+
+#X = np.reshape(x, (len(x), seq_len, 1))
+#X =X/ float(n_vocab)
+
+y = utils.to_categorical(y)
+
+print(y)
+
+
+
+
+model = Sequential()
+model.add(LSTM(256, input_shape = (X.shape[0], X.shape[1]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(256))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss = 'categorical_crossentropy', optimizer='adam')
 
 
 
